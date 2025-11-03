5946ab44aaabf4177953383906077d97caa16a4f
EmanuelMihai98 <mihai.degeratu98@gmail.com>
2025-11-03 22:50:17 +0200

    Cleaned temporary superuser

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,14 +10,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.auth.models import User
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
 
-
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "parola123")
-    print("Superuser creat cu succes!")
